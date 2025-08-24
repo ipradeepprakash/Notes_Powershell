a371acf7dc7f104854edeefe3834173244cfa874
Pradeep Prakash <pradeep_prakash@outlook.com>
2025-08-24 15:28:05 +0530

    added comments

--- Check - Disk Space Free.ps1
+++ Check - Disk Space Free.ps1
@@ -1,4 +1,8 @@
-﻿cls
+﻿
+
+# get disk drive details
+
+cls
 Get-Volume | Select-Object DriveLetter, 
 @{Name='SizeGB';Expression={[math]::Round($_.size / 1GB, 2)}}, 
 @{Name='FreeSpaceGB';Expression={[math]::Round($_.sizeremaining / 1GB, 2)}}
