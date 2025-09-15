e3b3fb21ccf7ba4bdef092c85032365a4621bf75
Pradeep Prakash <pradeep_prakash@outlook.com>
2025-09-15 23:12:24 +0530

    yes

---
+++ eBook -PowerShell 7 Workshop - Nick Parlow/1. Introduction to PowerShell 7 – What It Is and How to Get It.py
@@ -0,0 +1,64 @@
+<#
+This chapter is a general introduction to PowerShell 7. It’s going to give you some context about PowerShell and get you up and running. 
+In this chapter, we’re going to cover the following main topics:
+
+    What is PowerShell 7?
+    What is PowerShell 7 used for?
+    Getting PowerShell 7
+    Running PowerShell 7
+    Getting help
+
+What is PowerShell 7?
+
+    PowerShell is a scripting language and an alternative to the command-line interface. 
+    PowerShell is an automation tool consisting of (at least) 3 parts:
+
+
+        1. A shell, like the Command Prompt in Windows or the Terminal in Linux or macOS
+        2. A scripting language
+        3. A configuration management framework called Desired State Configuration (DSC)
+
+there are two main flavors of PowerShell: 
+1. Windows PowerShell: which comes bundled with both server and desktop versions of Windows, and 
+2. PowerShell 7: which must be downloaded and installed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#>
