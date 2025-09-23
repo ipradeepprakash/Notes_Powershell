42b941b8c6bb8dae8ce80e6a552a575e34882749
Pradeep Prakash <pradeep_prakash@outlook.com>
2025-09-23 20:30:06 +0530

    moved files2

---
+++ Powershell - Courses/eBook -PowerShell 7 Workshop - Nick Parlow/Table Contents.py
@@ -0,0 +1,67 @@
+<# 
+eBook -PowerShell 7 Workshop - Nick Parlow
+
+Preface
+---------
+The book is split into 3 parts. 
+
+Part 1
+------------------ 
+> we cover traditional coding theory; starting with how PowerShell works as a language, 
+we look at the building blocks of the language and then move on to how we can combine them in a programmatic flow.
+
+Part 2
+----------------- 
+> we start to put together the principles we’ve learned into scripts and modules that we can share and reuse.
+
+Part 3
+----------------- 
+> we look at how PowerShell works in different environments, 
+before finishing with a chapter on how we can access the underlying framework that PowerShell is built on.
+
+
+Part 1: PowerShell Fundamentals
+=================================
+    In this part, we’ll learn about the basics of PowerShell, how the language syntax works, how we can send the results of one operation straight 
+    into another with the pipeline, how PowerShell uses objects, variables, and data structures, how we can create branched and looped code, and 
+    finally, how we can write to and from stored files.
+
+        Chapter 1, Introduction to PowerShell – What It Is and How to Get It
+        Chapter 2, Exploring PowerShell Cmdlets and Syntax
+        Chapter 3, The PowerShell Pipeline – How to String Cmdlets Together
+        Chapter 4, PowerShell Variables and Data Structures
+        Chapter 5, PowerShell Control Flow – Conditionals and Loops
+        Chapter 6, PowerShell and Files – Reading, Writing, and Manipulating Data
+        Chapter 7, PowerShell and the Web – HTTP, REST, and JSON
+
+Part 2: Scripting and Toolmaking
+=====================================
+    This part walks you through turning a set of cmdlets into a script, introduces functional programming, shows you how to turn scripts into modules, and how to share those modules with colleagues and others using GitHub and GitLab. It also includes a chapter on PowerShell security, so that you don’t inadvertently distribute code that is less secure than it could be.
+
+        This part has the following chapters:
+
+        Chapter 8, Writing Our First Script – Turning Simple Cmdlets into Reusable Code
+        Chapter 9, Don’t Repeat Yourself – Functions and Scriptblocks
+        Chapter 10, Error Handling – Oh No! It’s Gone Wrong!
+        Chapter 11, Creating Our First Module
+        Chapter 12, Securing PowerShell
+
+Part 3: Using PowerShell
+=====================================
+    Now we understand how to use PowerShell 7, and how to write simple scripts and modules, it’s time to learn how to use it on different platforms. One of the defining features of PowerShell 7 is that it is cross-platform. This section will detail how to use PowerShell with a variety of platforms. This part has the following chapters:
+
+    Chapter 13, Working with PowerShell 7 and Windows
+    Chapter 14, PowerShell 7 for Linux and macOS
+    Chapter 15, PowerShell 7 and the Raspberry Pi
+    Chapter 16, Working with PowerShell and .NET
+
+
+
+
+
+
+
+
+
+
+
